32540cac3b8a524f29ab481709d6d4c16c4b8db4
unknown <nknittin16436@gmail.com>
2022-08-25 12:07:26 +0530

    Added Dashbaord menu

--- sih/src/pages/dashboard/dashboard.jsx
+++ sih/src/pages/dashboard/dashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import DashboardMenu from "./components/DashboardMenu/dashboardMenu";
 // import DashboardMainMenu from "./main__menu__dashboard/DashboardMainMenu";
 
 const dashboardHome = () => {
   return (
     <div className="dashboard">
       <div className="dashboard__body">
+        <DashboardMenu />
         <div className="dashboard__body__homepage">
           <Outlet />
         </div>
